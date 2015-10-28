3693bcf345e515f4208196c4cfb10a006084d2c3
Wojtek Siudzinski <admin@suda.pl>
2015-10-28 17:52:58 +0000

    No longer ignore exit code

--- scripts/build-all-platforms.sh
+++ scripts/build-all-platforms.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Pre-build all possible platforms
 PLATFORMS=`grep -Pow "PLATFORM_ID\s?=\s?(\d+)$" /firmware/build/platform-id.mk`
-cd /firmware/modules
+cd /firmware/main
 while read -r PLATFORM; do
 	PLATFORM="$(tr -d " " <<<$PLATFORM)"
 	eval "$PLATFORM make"
 done <<< "$PLATFORMS"
-# Return with code 0 to ignore "Platform X does not support dynamic modules" error
-exit 0
