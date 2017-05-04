ec952e3cff395d104c16c657ddbf881d79c1d7fa
Julien Vanier <julien@particle.io>
2017-05-04 00:39:55 -0400

    Another build released

--- build_manual.sh
+++ build_manual.sh
@@ -3,7 +3,7 @@
 set -xe
 
 export DOCKER_IMAGE_NAME=particle/buildpack-particle-firmware
-export TRAVIS_TAG=0.5.1-pi.12
+export TRAVIS_TAG=0.5.1-pi.13
 
 rsync -a --exclude .git --exclude 'build/target' /home/monkbroc/Programming/firmware-rpi/* firmware/
 cd firmware
